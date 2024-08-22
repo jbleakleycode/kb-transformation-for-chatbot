3095db83753439d62b186a0a6f26b31daacd9a07
jacobbleakley-neo4j <jacob.bleakley@neo4j.com>
2024-08-22 10:26:54 +0100

    prints ontologies as one string separated by ENDn

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from openai import OpenAI
 
-# client = OpenAI
+client = OpenAI
 
-# def openai_call(prompt):
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo-0125",
-#         messages=[
-#             {"role": "system", "content": "You are an assistant helping to identify labels for the document which will be applied to an ontology. You will be provided the document and list of labels as well as the ontology they belong to. Please provide the response in the format 'label1, label2, label3' etc where label1, label2 and label3 DO apply to the document. Labels must have same spelling, spacing and casing as what it provided since clean data is necessary for querying later on. Do not give any extra explaination or content in the response."},
-#             {"role": "user", "content": prompt}
-#         ]
-#     )
-#     return response.choices[0].message.content 
+def openai_call(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "You are an assistant helping to identify labels for the document which will be applied to an ontology. You will be provided the document and list of labels as well as the ontology they belong to. Please provide the response in the format 'label1, label2, label3' etc where label1, label2 and label3 DO apply to the document. Labels must have same spelling, spacing and casing as what it provided since clean data is necessary for querying later on. Do not give any extra explaination or content in the response."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content 
 
 import pandas as pd
 
@@ -28,19 +28,37 @@ def getOntologyStrings():
     content_type_ontologies = df['Content Type'].dropna().tolist()
     editions_ontologies = df['Editions'].dropna().tolist()
     
-    # Convert the list to a string
-    team_string = "Team: " + ", ".join(team_ontologies) + " END"
-    cd_string = "Content Distribution: " + ", ".join(content_distribution_ontologies) + " END"
-    industry_string = "Industry: " + ", ".join(industry_ontologies) + " END"
-    use_case_string = "Use-Case: " + ", ".join(use_case_ontologies) + " END"
-    cm_string = "Content Maturity: " + ", ".join(content_maturity_ontologies) + " END"
-    ct_string = "Content Type: " + ", ".join(content_type_ontologies) + " END"
-    editions_string = "Editions: " + ", ".join(editions_ontologies) + " END"
     
-    return team_string, cd_string, industry_string, use_case_string, cm_string, ct_string, editions_string
+    # Convert the lists to strings and concatenate them with newlines
+    ontology_string = (
+        "Team: " + ", ".join(team_ontologies) + " END\n" +
+        "Content Distribution: " + ", ".join(content_distribution_ontologies) + " END\n" +
+        "Industry: " + ", ".join(industry_ontologies) + " END\n" +
+        "Use-Case: " + ", ".join(use_case_ontologies) + " END\n" +
+        "Content Maturity: " + ", ".join(content_maturity_ontologies) + " END\n" +
+        "Content Type: " + ", ".join(content_type_ontologies) + " END\n" +
+        "Editions: " + ", ".join(editions_ontologies) + " END"
+    )
+    
+    return ontology_string
 
 if __name__ == "__main__":
-    strings = getOntologyStrings()
-    for string in strings:
-        print(string + "\n")
+    ontology_string = getOntologyStrings()
+    print(ontology_string)
+    
+#     # Convert the list to a string
+#     team_string = "Team: " + ", ".join(team_ontologies) + " END"
+#     cd_string = "Content Distribution: " + ", ".join(content_distribution_ontologies) + " END"
+#     industry_string = "Industry: " + ", ".join(industry_ontologies) + " END"
+#     use_case_string = "Use-Case: " + ", ".join(use_case_ontologies) + " END"
+#     cm_string = "Content Maturity: " + ", ".join(content_maturity_ontologies) + " END"
+#     ct_string = "Content Type: " + ", ".join(content_type_ontologies) + " END"
+#     editions_string = "Editions: " + ", ".join(editions_ontologies) + " END"
+    
+#     return team_string, cd_string, industry_string, use_case_string, cm_string, ct_string, editions_string
+
+# if __name__ == "__main__":
+#     strings = getOntologyStrings()
+#     for string in strings:
+#         print(string + "\n")
 
